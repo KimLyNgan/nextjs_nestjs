37cea04468d40b7653e17b5ca85e58c7566d1234
AnHii <thuongkhungcuukyquyet@gmail.com>
2025-07-17 16:33:02 +0700

    connect to mongo db

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,4 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(8080);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('Failed to bootstrap the application', err);
+  process.exit(1);
+});
