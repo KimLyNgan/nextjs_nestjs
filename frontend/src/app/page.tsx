eaaf85ac510f90579d51d1a246ef7d2970e4ba46
AnHii <thuongkhungcuukyquyet@gmail.com>
2025-07-17 14:57:15 +0700

    add ant design to FE

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,13 @@
+import "@ant-design/v5-patch-for-react-19";
+import { Button } from "antd";
+
 export default function Home() {
-   return <div>hello world</div>;
+   return (
+      <div>
+         hello world
+         <div>
+            <Button type="primary">Button</Button>
+         </div>
+      </div>
+   );
 }
